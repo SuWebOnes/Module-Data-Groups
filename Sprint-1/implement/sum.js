0ba1d5f2e86dd735634b81869ba6445698108b19
SuWebOnes <surafealaw@gmail.com>
2025-07-19 01:15:56 +0100

    update: fix  function code to implement a function that sums the numerical elements of an array

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,9 @@
 function sum(elements) {
+  if (!Array.isArray(elements)) return 0;
+
+  return elements.reduce((acc, val) => {
+    return typeof val === "number" && !isNaN(val) ? acc + val : acc;
+  }, 0);
 }
 
 module.exports = sum;
