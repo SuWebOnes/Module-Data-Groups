ef76a1f0cfe723af0d6e694cd2c3df2bd04dc693
SuWebOnes <surafealaw@gmail.com>
2025-07-19 01:04:15 +0100

    update: fix code for implement a function that find the largest numerical element of an array

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,11 @@
+
+
 function findMax(elements) {
+  if (!Array.isArray(elements)) return -Infinity;
+
+  const nums = elements.filter(val => typeof val === 'number' && !isNaN(val));
+
+  return nums.length > 0 ? Math.max(...nums) : -Infinity;
 }
 
 module.exports = findMax;
