ba34de636aa6a089010bc1ec58c9643ec0162723
SuWebOnes <surafealaw@gmail.com>
2025-07-21 21:59:00 +0100

    update: fix function code for tally based on the test requirements tally.js

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,15 @@
-function tally() {}
+function tally(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("Input must be an array");
+  }
+
+  const counts = {};
+
+  for (const item of arr) {
+    counts[item] = (counts[item] || 0) + 1;
+  }
+
+  return counts;
+}
 
 module.exports = tally;
