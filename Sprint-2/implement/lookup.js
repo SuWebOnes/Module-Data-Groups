9667024cc11af855a330bb522a54963f88087ea4
SuWebOnes <surafealaw@gmail.com>
2025-07-21 15:43:01 +0100

    update code function for lockup

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,9 @@
-function createLookup() {
-  // implementation here
+function createLookup(pairs) {
+  const result = {};
+  for (const [country, currency] of pairs) {
+    result[country] = currency;
+  }
+  return result;
 }
 
 module.exports = createLookup;
