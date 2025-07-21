fcb6576520521a12e9d5915316160201b35bd840
SuWebOnes <surafealaw@gmail.com>
2025-07-21 22:29:39 +0100

    update: Test  invert for case inverts multiple key-value pairs in sprint-2 interpret invert.test.js

--- Sprint-2/interpret/invert.test.js
+++ Sprint-2/interpret/invert.test.js
@@ -8,3 +8,8 @@ test("returns empty object when given an empty object", () => {
 test("inverts a single key-value pair", () => {
   expect(invert({ a: 1 })).toEqual({ 1: "a" });
 });
+
+// Test: multiple key-value pairs
+test("inverts multiple key-value pairs", () => {
+  expect(invert({ a: 1, b: 2 })).toEqual({ 1: "a", 2: "b" });
+});
