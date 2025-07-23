1699cc6615e13ee639cae87f4268076a26ac8dfc
SuWebOnes <surafealaw@gmail.com>
2025-07-23 13:57:06 +0100

    update: code for setting countdown and play audio

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,5 +1,38 @@
-function setAlarm() {}
+let timeRemaining = 0;
+let intervalId;
 
+function setAlarm() {
+  // Get the input value (number of seconds)
+  const input = document.getElementById("alarmSet");
+  timeRemaining = parseInt(input.value, 10);
+
+  // Immediately update the display
+  updateTimeDisplay();
+
+  // Clear any existing timer
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+
+    // Set up the countdown timer
+  intervalId = setInterval(() => {
+    timeRemaining--;
+
+    updateTimeDisplay();
+
+    if (timeRemaining <= 0) {
+      clearInterval(intervalId);
+      playAlarm();
+    }
+  }, 1000);
+}
+
+function updateTimeDisplay() {
+  const heading = document.getElementById("timeRemaining");
+  const minutes = Math.floor(timeRemaining / 60).toString().padStart(2, "0");
+  const seconds = (timeRemaining % 60).toString().padStart(2, "0");
+  heading.innerText = `Time Remaining: ${minutes}:${seconds}`;
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
