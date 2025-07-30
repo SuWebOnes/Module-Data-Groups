1e1b689623e50201e70faf2f345bff24117db2c6
SuWebOnes <surafealaw@gmail.com>
2025-07-31 00:43:52 +0100

    update: fix slideshow app to pass the tests in slideshow.js

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,80 +1,55 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
 ];
 
+let current = 0;
+let intervalId = null;
 
-// Write your code here
-// Start at the first image
-let currentIndex = 0;
-let intervalId = null; // To track interval for auto-slideshow
+const img = document.getElementById("carousel-img");
+const forward = document.getElementById("forward-btn");
+const backward = document.getElementById("backward-btn");
+const autoForward = document.getElementById("auto-forward");
+const autoBack = document.getElementById("auto-backward");
+const stop = document.getElementById("stop");
 
-// Get DOM elements
-const image = document.querySelector("#carousel-img");
-const forwardBtn = document.querySelector("#forward-btn");
-const backwardBtn = document.querySelector("#backward-btn");
-const autoForwardBtn = document.querySelector("#auto-forward");
-const autoBackBtn = document.querySelector("#auto-backward");
-const stopBtn = document.querySelector("#stop");
-
-// ========== Core Functionality ==========
-
-// Update the image based on currentIndex
 function updateImage() {
-  image.src = images[currentIndex];
+  img.src = images[current];
 }
 
-// Move forward once
-function moveForward() {
-  currentIndex = (currentIndex + 1) % images.length;
+forward.addEventListener("click", () => {
+  current = (current + 1) % images.length;
   updateImage();
-}
+});
 
-// Move backward once
-function moveBackward() {
-  currentIndex = (currentIndex - 1 + images.length) % images.length;
+backward.addEventListener("click", () => {
+  current = (current - 1 + images.length) % images.length;
   updateImage();
-}
+});
 
-// Start auto-slideshow forward
-function startAutoForward() {
-  disableButtons();
-  intervalId = setInterval(moveForward, 2000);
-}
+autoForward.addEventListener("click", () => {
+  disableAutoButtons(true);
+  intervalId = setInterval(() => {
+    current = (current + 1) % images.length;
+    updateImage();
+  }, 2000);
+});
 
-// Start auto-slideshow backward
-function startAutoBackward() {
-  disableButtons();
-  intervalId = setInterval(moveBackward, 2000);
-}
+autoBack.addEventListener("click", () => {
+  disableAutoButtons(true);
+  intervalId = setInterval(() => {
+    current = (current - 1 + images.length) % images.length;
+    updateImage();
+  }, 2000);
+});
 
-// Stop the slideshow
-function stopSlideshow() {
+stop.addEventListener("click", () => {
   clearInterval(intervalId);
-  intervalId = null;
-  enableButtons();
+  disableAutoButtons(false);
+});
+
+function disableAutoButtons(disable) {
+  autoForward.disabled = disable;
+  autoBack.disabled = disable;
 }
-
-// Disable auto buttons during auto-play
-function disableButtons() {
-  autoForwardBtn.disabled = true;
-  autoBackBtn.disabled = true;
-}
-
-// Re-enable auto buttons
-function enableButtons() {
-  autoForwardBtn.disabled = false;
-  autoBackBtn.disabled = false;
-}
-
-// ========== Event Listeners ==========
-
-forwardBtn.addEventListener("click", moveForward);
-backwardBtn.addEventListener("click", moveBackward);
-autoForwardBtn.addEventListener("click", startAutoForward);
-autoBackBtn.addEventListener("click", startAutoBackward);
-stopBtn.addEventListener("click", stopSlideshow);
-
-// ========== Initial Setup ==========
-updateImage();
